6718ae0e469651ad4bb92617df7bf0da0fec2ef5
anselm <anselm@gmail.com>
2025-01-02 22:26:23 -0800

    more flexible resizing support for renderer

--- handlers/scene.js
+++ handlers/scene.js
@@ -4,7 +4,7 @@ const uuid = 'orbital/orbital-volume/scene'
 import { getThree } from './three-helper.js'
 
 //
-// handle scene related events such as finding a rendering div and setting up a camera
+// handle scene related events such as finding a rendering div and setting up a camera and updates
 //
 
 export default async function scene(sys,surface,entity,delta) {
@@ -48,60 +48,59 @@ export default async function scene(sys,surface,entity,delta) {
 		return
 	}
 
-	// only run once
+	// only run once for now - allow revisions in the future @todo
 	if(surface._built) return
 	surface._built = true
 
 	//
-	// find or build a parent dom element that the 3d view will be attached to
+	// find a dom element to bind to by name and build renderer
 	//
 
 	let div = surface.div = document.getElementById(surface.name)
+
 	if(!div) {
 		div = surface.div = document.createElement("div")
 		div.style = "width:100%;height:100%;padding:0px;margin:0px;position:absolute;top:0;left:0;"
 		div.id = surface.name
 		document.body.appendChild(div)
 	}
-	const width = div.clientWidth
-	const height = div.clientHeight
-
-	//
-	// build renderer with its own dom element that will be appended to the div
-	//
-
 	const alpha = volume.hasOwnProperty('alpha') ? volume.alpha : false
 	const background = volume.hasOwnProperty('background') ? volume.background : 0x000000
-
-
-	const renderer = surface.renderer = new THREE.WebGLRenderer({
+	const render_args = {
 		antialias: false,
 		preserveDrawingBuffer: true,
-		alpha
-	})
+		alpha,
+	}
+
+	const canvas = div instanceof HTMLCanvasElement ? div : null
+	if(canvas) {
+		render_args.canvas = canvas
+	}
+
+	const renderer = surface.renderer = new THREE.WebGLRenderer(render_args)
+
+	// this is an issue for transparent backdrops - disabled for now
 	//renderer.autoClearColor = false
+
+	// for transparent backdrops must not set clear color
 	if(background != 'transparent') {
-		// for alpha must not set this
 		renderer.setClearColor(background)
 	}
-	renderer.setSize(width,height)
-	renderer.setPixelRatio(window.devicePixelRatio)
 
 	//
-	// nicer rendering - off for now due to alpha transparency desires
+	// Better low level color space support - also causes issues with transparent backdrops
 	//
 
 	if(volume.prettier) {
 		surface.renderer.outputColorSpace = THREE.SRGBColorSpace
 		surface.renderer.outputEncoding = THREE.sRGBEncoding
 		surface.renderer.toneMapping = THREE.ACESFilmicToneMapping
-		surface.renderer.shadowMap.enabled = false
+		surface.renderer.shadowMap.enabled = true
 		surface.renderer.useLegacyLights = false
 	}
-	div.append(renderer.domElement)
 
 	//
-	// build a 3js scene node which is mandatory
+	// build scene
 	//
 
 	const scene = surface.scene = new THREE.Scene()
@@ -110,10 +109,10 @@ export default async function scene(sys,surface,entity,delta) {
 	}
 
 	//
-	// Default room lighting
+	// Scene lighting feature - this causes issues with transparent backdrops
 	//
 
-	if(false) {
+	if(volume.roomlighting) {
 		const RoomEnvironment = (await import('three/addons/environments/RoomEnvironment.js')).RoomEnvironment
 		const pmremGenerator = new THREE.PMREMGenerator( renderer )
 		pmremGenerator.compileEquirectangularShader()
@@ -121,18 +120,27 @@ export default async function scene(sys,surface,entity,delta) {
 	}
 
 	//
-	// always have a default camera - can be overridden
+	// adjust renderer size/aspect
+	//
+
+	surface.width = div.clientWidth || volume.width || 512
+	surface.height = div.clientHeight || volume.height || 512
+	surface.aspect = surface.width / surface.height
+	//renderer.setSize(surface.width,surface.height)
+	//renderer.setPixelRatio(window.devicePixelRatio)
+
+	//
+	// build default camera
 	//
 
 	const fov = volume.aperture || 35
 	const near = volume.near || 0.01
 	const far = volume.far || 100
-	const aspect = width / height
-	const camera = surface.camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
+	const camera = surface.camera = new THREE.PerspectiveCamera(fov, surface.aspect, near, far)
 	scene.add(camera)
 
 	//
-	// helpers
+	// other helpers
 	//
 
 	if(volume.axes && THREE.GridHelper && THREE.AxesHelper) {
@@ -141,26 +149,30 @@ export default async function scene(sys,surface,entity,delta) {
 	}
 
 	//
-	// attach this all to the display
-	//
-
-	div.appendChild(renderer.domElement)
-
-	//
-	// a resize observer leveraging https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver
+	// resize observer leveraging https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver
 	//
 
 	const resized = () => {
-		surface.width = div.clientWidth
-		surface.height = div.clientHeight
-		if(surface.camera && surface.scene) {
-			surface.camera.aspect = width / height
+		if(!div.clientWidth || !div.clientHeight) return
+		surface.width = div.clientWidth || volume.width || 512
+		surface.height = div.clientHeight || volume.height || 512
+		surface.aspect = surface.width / surface.height
+		if(surface.camera) {
+			surface.camera.aspect = surface.aspect
 			surface.camera.updateProjectionMatrix()
-			renderer.setSize(width,height)
-			renderer.render(surface.scene,surface.camera)
 		}
+		renderer.setSize(surface.width,surface.height)
+		renderer.render(surface.scene,surface.camera)
 	}
 
 	surface.resizeObserver = new ResizeObserver(resized).observe(div)
+
+	//
+	// attach this all to the display
+	//
+
+	if(!canvas) {
+		div.appendChild(renderer.domElement)
+	}
 }
 
